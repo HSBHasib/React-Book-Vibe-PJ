04334f6aa6e2e4e909625c32af0cf8754d67765b
Hasib <hasibhsb19@gmail.com>
2026-04-06 21:49:31 +0600

    add navber

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,26 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import MainLayout from './layout/MainLayout.jsx';
+import Navber from './components/Navber/Navber.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: MainLayout,
+    children: [
+      {index: true, Component: Navber},
+      // {path: '/dashboard', Component: }
+    ]
+    
+  },
+]);
+
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <RouterProvider router={router}>
     <App />
-  </StrictMode>,
+  </RouterProvider>,
 )
