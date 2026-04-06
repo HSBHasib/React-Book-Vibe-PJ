8e52c2b9287404baea5c280ec74932f0411fb6ec
Hasib <hasibhsb19@gmail.com>
2026-04-06 21:57:14 +0600

    update navber desing

--- src/components/Navber/Navber.jsx
+++ src/components/Navber/Navber.jsx
@@ -4,18 +4,24 @@ const Navber = () => {
   return (
     <div>
       <div className="navbar bg-base-100 shadow-sm">
-        <div className="navbar-start">
+        <div className="navbar-start -space-x-3">
             <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
-            </div>
-            <ul
-                tabIndex="-1"
-                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow font-semibold">
-                <li><NavLink>Home</NavLink></li>
-                <li><NavLink>Listed Books</NavLink></li>
-                <li><NavLink>Page to Read</NavLink></li>
-            </ul>
+                <div tabIndex={0} role="button" className="btn btn-ghost border-none lg:hidden">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 active:scale-95" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                </div>
+                <ul
+                    tabIndex="-1"
+                    className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow font-semibold">
+                    <li><NavLink to='/' className={({isActive}) => `${
+                        isActive ? 'text-green-500 border border-green-500 rounded-md' : ''
+                    }`}>Home</NavLink></li>
+                    <li><NavLink to='/page' className={({isActive}) => `${
+                        isActive ? 'text-green-500 border border-green-500 rounded-md' : ''
+                    }`}>Listed Books</NavLink></li>
+                    <li><NavLink to='/page2' className={({isActive}) => `${
+                        isActive ? 'text-green-500 border border-green-500 rounded-md' : ''
+                    }`}>Page to Read</NavLink></li>
+                </ul>
             </div>
             <a className="text-xl font-bold">Book Vibe</a>
         </div>
@@ -42,3 +48,4 @@ const Navber = () => {
 }
 
 export default Navber
+
