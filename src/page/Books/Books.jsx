d1b8b9c0a9a52dea51097e6db4720fe4459781d4
Hasib <hasibhsb19@gmail.com>
2026-04-08 21:10:39 +0600

    add dynamic value on listed cards

--- src/page/Books/Books.jsx
+++ src/page/Books/Books.jsx
@@ -4,16 +4,23 @@ import ListedReadList from '../../components/ListedBooks/ListedReadList';
 import ListedWishList from '../../components/ListedBooks/ListedWishList';
 import { useContext } from 'react';
 import { BookContext } from '../../context/BookContext';
+import { FaAngleDown } from 'react-icons/fa6';
 
 
 const Books = () => {
-  const {readList, WishList} = useContext(BookContext);
 
   return (
-    <div className='w-[90%] mx-auto'>
-        <div >
+    <div className='w-[90%] mx-auto space-y-2'>
+        <div className='text-center'>
             <h1 className='bg-gray-100 py-4 my-4 flex justify-center font-bold text-2xl rounded-xl'>Books</h1>
             
+            <div className="dropdown dropdown-center">
+            <div tabIndex={0} role="button" className="btn m-1 text-white bg-[#23BE0A] rounded-lg border-none">Sort By <FaAngleDown /></div>
+                <ul tabIndex="-1" className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+                    <li><a>Item 1</a></li>
+                    <li><a>Item 2</a></li>
+                </ul>
+            </div>
         </div>
        <Tabs>
             <TabList className='mb-4'>
--- src/ui/ListedCards.jsx
+++ src/ui/ListedCards.jsx
@@ -19,45 +19,45 @@ const ListedCards = ({book}) => {
             {/* Content Section */}
             <div className="flex-grow w-full">
             <h2 className="text-3xl font-serif font-bold text-[#131313] mb-2">
-                The Catcher in the Rye
+                {book.bookName}
             </h2>
             <p className="text-[#131313cc] text-[16px] mb-4">
-                By : <span className="font-semibold">Awlad Hossain</span>
+                By : <span className="font-semibold">{book.author}</span>
             </p>
             {/* Tags Section */}
             <div className="flex flex-wrap items-center gap-4 mb-5">
                 <span className="font-bold text-[#131313] mr-2">Tag</span>
                 <span className="bg-[#23BE0A0D] text-[#23BE0A] px-5 py-2 rounded-full text-base font-medium">
-                #Young Adult
+                #{book.tags[0]}
                 </span>
                 <span className="bg-[#23BE0A0D] text-[#23BE0A] px-5 py-2 rounded-full text-base font-medium">
-                #Identity
+                #{book.tags[1]}
                 </span>
                 
                 <div className="flex items-center text-[#131313cc] gap-2 ml-auto md:ml-4">
                 <SlLocationPin size={22} />
-                Year of Publishing: 1924
+                Year of Publishing: {book.yearOfPublishing}
                 </div>
             </div>
             {/* Publisher Info */}
             <div className="flex flex-wrap items-center gap-8 mb-5 text-[#13131399]">
                 <div className="flex items-center gap-1.5 text-[15px]">
                 <GoPeople size={22}/>
-                Publisher: Scribner
+                Publisher: {book.publisher}
                 </div>
                 <div className="flex items-center gap-1.5 text-[15px]">
                 <BiMemoryCard size={22}/>
-                Page 192
+                Page {book.totalPages}
                 </div>
             </div>
             <hr className="border-gray-300 mb-4" />
             {/* Action Footer */}
             <div className="space-x-5">
                 <span className="bg-[#328EFF26] text-[15px] text-[#328EFF] px-6 py-2.5 rounded-full ">
-                Category: Classic
+                Category: {book.category}
                 </span>
                 <span className="bg-[#FFAC3326] text-[15px] text-[#FFAC33] px-6 py-2.5 rounded-full ">
-                Rating: 4.5
+                Rating: {book.rating}
                 </span>
                 <button className="bg-[#23BE0A] text-[15px] text-white px-7 py-2.5 rounded-full font-semibold hover:bg-[#1a8d07] transition-all active:scale-95 ml-auto">
                 View Details
