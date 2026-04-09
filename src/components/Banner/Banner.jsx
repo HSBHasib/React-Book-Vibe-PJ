895ab44712063760540063f0fc0a3033241f8d07
Hasib <hasibhsb19@gmail.com>
2026-04-09 14:03:11 +0600

    add one more rountes on banner btn to navigate home to listedBooks section

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,6 +1,10 @@
+import { useNavigate } from 'react-router'
 import bannerImg from '../../assets/img/bannerImg.png'
 
 const Banner = () => {
+
+  const navigate = useNavigate();
+
   return (
     <div>
       <div className="hero bg-base-200 min-screen-[60vh] my-6">
@@ -11,7 +15,7 @@ const Banner = () => {
             />
             <div className='space-y-7'>
               <h1 className="sm:text-5xl text-4xl font-bold sm:leading-14">Books to freshen up <br /> your bookshelf</h1>
-              <button className="btn bg-[#23BE0A] text-white font-bold rounded-lg py-5 active:scale-95">View The List</button>
+              <button onClick={() => navigate('/listedBooks')} className="btn bg-[#23BE0A] text-white font-bold rounded-lg py-5 active:scale-95">View The List</button>
             </div>
         </div>
         </div>
